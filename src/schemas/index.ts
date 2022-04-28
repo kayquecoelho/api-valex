abecf0148403fb53fc6e348e2331af9d4aea4173
Kayque <lkcoelho76@gmail.com>
2022-04-27 22:01:15 -0300

    refactor: create folder to group card schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,10 +1,10 @@
-import activateCardSchema from "./activateCardSchema.js";
-import cardRequestSchema from "./cardRequestSchema.js";
+import activateCardSchema from "./cardSchemas/activateCardSchema.js";
+import cardRequestSchema from "./cardSchemas/cardRequestSchema.js";
 import rechargesSchema from "./rechargesSchema.js";
 import paymentSchema from "./paymentSchema.js";
-import blockCardSchema from "./blockCardSchema.js";
+import blockCardSchema from "./cardSchemas/blockCardSchema.js";
 import onlinePaymentSchema from "./onlinePaymentSchema.js";
-import digitalCardSchema from "./digitalCardSchema.js";
+import digitalCardSchema from "./cardSchemas/digitalCardSchema.js";
 
 const schemas = {
   activateCardSchema,
